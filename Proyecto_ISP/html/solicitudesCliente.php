8d4e2e1a865af0c35d9f95410f65f6444cb26310
AndresChac <a.chacon@cognitiva.la>
2018-07-10 21:37:06 -0400

    Comenzando

--- Proyecto_ISP/html/solicitudesCliente.php
+++ Proyecto_ISP/html/solicitudesCliente.php
@@ -59,51 +59,42 @@
                     </ul>
                 </div>
             </nav>
-            
             <div class="container-fluid">
-                <h3>Noticias</h3>
                 <div class="row">
                     <div class="col">
                         <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title">Nuevo sistema</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Para todos nuestros clientes</h6>
-                                <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
-                                <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
-                                <a href="http://websitedesigntamilnadu.com" class="card-link">Another</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Try Other</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6>
-                                <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
-                                <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
-                                <a href="http://websitedesigntamilnadu.com" class="card-link">Another link</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Try Other</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6>
-                                <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
-                                <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
-                                <a href="http://websitedesigntamilnadu.com" class="card-link">Another link</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Try Other</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6>
-                                <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
-                                <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
-                                <a href="http://websitedesigntamilnadu.com" class="card-link">Another link</a>
+                                <h5 class="card-title">Mis muestras</h5>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Codigo</th>
+                                            <th scope="col">Cantidad</th>
+                                            <th scope="col">Fecha ingreso</th>
+                                            <th scope="col">Estado</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">3</th>
+                                            <td>Larry</td>
+                                            <td>the Bird</td>
+                                            <td>@twitter</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
